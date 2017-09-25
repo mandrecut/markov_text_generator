1b81d1884ca638acd86794294ef91b05cca4f85c
mandrecut <mircea.andrecut@gmail.com>
2017-09-25 09:26:23 -0600

    Version 0.5

--- test.py
+++ test.py
@@ -3,27 +3,17 @@ import textwrap
 from markov import MarkovChar, MarkovWord
 
 if __name__ == '__main__':
-
     fname = "./datatxt/sherlock_holmes.txt"
-
     if(sys.version_info[0] < 3):
-        # Python 2	
         with open(fname, "r") as f:
             x = f.read().decode("UTF-8")
     else:
-        # Python 3
         with open(fname, "r", encoding="utf-8") as f:
             x = f.read()
-
     mc = MarkovChar(6)
-
     mc.learn(x)
-
     print(textwrap.fill(mc.generate(500), width=72))
-
     mw = MarkovWord(2)
-
     mw.learn(x)
-
     print(textwrap.fill(mw.generate(100), width=72))
 
